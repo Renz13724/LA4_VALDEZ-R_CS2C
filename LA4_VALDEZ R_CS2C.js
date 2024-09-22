0afbd4c116da77a015fff54dd5cb078357e46a05
VALDEZ R. <renzv753@gmail.com>
2024-09-22 17:24:42 +0800

    Create LA4_VALDEZ R_CS2C.js

---
+++ LA4_VALDEZ R_CS2C.js
@@ -0,0 +1,14 @@
+//Comparison or Relational Operator
+let firstFavNumb = 13;
+let secondFavNumb = 10;
+
+console.log(firstFavNumb > secondFavNumb);
+console.log(firstFavNumb < secondFavNumb);
+console.log(firstFavNumb >= secondFavNumb);
+console.log(firstFavNumb <= secondFavNumb);
+console.log(firstFavNumb === secondFavNumb);
+console.log(firstFavNumb == secondFavNumb);
+console.log(firstFavNumb !== secondFavNumb);
+console.log(firstFavNumb != secondFavNumb);
+console.log("firstFavNumb:", firstFavNumb);
+console.log("secondFavNumb", secondFavNumb);
